7065aa4cb03826f1bebe6468e0417928ea9bdba2
namqh <qhnam.67@gmail.com>
2025-02-03 11:38:20 +0700

    add env

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
 import express, { Application } from 'express';
+import dotenv from 'dotenv';
 
-const bootstrap = async () => {};
+dotenv.config();
+
+const bootstrap = async () => {
+  const app = express();
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+};
+
+bootstrap();
