4009b75b0e1f57eb4cddf45a6f29152740658195
namqh <qhnam.67@gmail.com>
2025-02-03 12:36:49 +0700

    add log request middleware

---
+++ src/common/middlewares/logger.middleware.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from 'express';
+
+export const loggerMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+};
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ dotenv.config();
 
 import express from 'express';
 import { DatabaseService } from './config/database.service';
+import { loggerMiddleware } from './common/middlewares/logger.middleware';
 
 const bootstrap = async () => {
   const app = express();
@@ -10,9 +11,18 @@ const bootstrap = async () => {
   // Initialize TypeORM
   await DatabaseService.initialize();
 
+  app.use(express.json());
+  app.use(loggerMiddleware);
+
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+  });
+
+  app.get('/test', (req, res) => {
+    res.send({
+      a: 1,
+    });
   });
 };
 
